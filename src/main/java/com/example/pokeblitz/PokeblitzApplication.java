98c043cf0e6378dffbd2e186530d2880a030f060
Vytis Lasaitis <v.lasaitis@gmail.com>
2023-01-23 14:04:07 +0100

    initial project

---
+++ src/main/java/com/example/pokeblitz/PokeblitzApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokeblitz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeblitzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeblitzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pokeblitz/PokeblitzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokeblitz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeblitzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
