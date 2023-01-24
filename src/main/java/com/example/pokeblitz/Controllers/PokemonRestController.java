4c2c81d829cd53a4e1949a5426f3a603d8e3608a
RasmusClausen <rasmusclausen123@gmail.com>
2023-01-24 09:59:00 +0100

    hej

---
+++ src/main/java/com/example/pokeblitz/Controllers/PokemonRestController.java
@@ -0,0 +1,10 @@
+package com.example.pokeblitz.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class PokemonRestController {
+
+}
